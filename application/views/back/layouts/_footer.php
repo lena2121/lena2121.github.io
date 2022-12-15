6e59b91cc4aeed6900204e7f5a1de87997b4de6a
lena <sitijulena07@gmail.com>
2022-12-15 15:57:15 +0700

    first commit

---
+++ application/views/back/layouts/_footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+   <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+      <span>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Repost by SitiJulena
+		</span>
+      </div>
+   </div>
+</footer>
